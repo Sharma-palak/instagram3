52ccbc4e57dd46bda0de9e023bebd87252123186
Sharma-palak <abhiruchi2015@gmail.com>
2020-06-22 21:34:00 +0530

    d

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import datetime
 import django_heroku
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -198,5 +198,5 @@ AUTH_USER_MODEL = "insta.User"
 APPEND_SLASH=False
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
