8c991178918e4d9b56ac5b4f95786a58c18b1997
Sharma-palak <abhiruchi2015@gmail.com>
2020-06-22 19:46:13 +0530

    added procfile

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -194,3 +195,5 @@ EMAIL_HOST_PASSWORD = 'year2020'
 EMAIL_PORT = 587
 AUTH_USER_MODEL = "insta.User"
 APPEND_SLASH=False
+# Activate Django-Heroku.
+django_heroku.settings(locals())
